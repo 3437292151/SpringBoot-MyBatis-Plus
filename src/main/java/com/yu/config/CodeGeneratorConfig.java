42b54aa7dbdc9587bb47d0660ac29acfc4d07077
yuchanglong <yuchanglong@epipechina.com>
2019-08-12 18:03:21 +0800

    springboot-mybatis Plus 代码生成器实现

--- src/main/java/com/yu/config/CodeGeneratorConfig.java
+++ src/main/java/com/yu/config/CodeGeneratorConfig.java
@@ -1,13 +1,19 @@
 package com.yu.config;
 
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Auther: yuchanglong
@@ -25,7 +31,9 @@ public class CodeGeneratorConfig {
         GlobalConfig gc = new GlobalConfig();
         String projectPath = System.getProperty("user.dir");
         gc.setOutputDir(projectPath + "/src/main/java");
-        gc.setAuthor("jobob");
+        gc.setAuthor("yuchanglong");
+        /*gc.setMapperName("Dao");
+        gc.setControllerName("Resource");*/
         gc.setOpen(false);
         return gc;
     }
@@ -48,7 +56,71 @@ public class CodeGeneratorConfig {
         PackageConfig pc = new PackageConfig();
         pc.setModuleName("");
         pc.setParent("com.yu");
+        pc.setEntity("domain");
+        pc.setController("web.rest");
+        pc.setMapper("dao");
         return pc;
+    }
+
+    @Bean
+    public InjectionConfig getInjectionConfig(){
+        String property = System.getProperty("user.dir");
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+
+
+        // 自定义输出配置
+        List<FileOutConfig> focList = new ArrayList<>();
+        // 如果模板引擎是 freemarker
+        String templatePath = "/templates/mapper.xml.ftl";
+        // 自定义配置会被优先输出
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return property + "/src/main/resources/mapper/"
+                        + tableInfo.getEntityName() + "Dao" + StringPool.DOT_XML;
+            }
+        });
+        templatePath = "/templates/mymapper.java.ftl";
+
+        // 自定义配置会被优先输出
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return property + "/src/main/" +
+                        getPackageConfig().getParent().replace(".","/")
+                        + "/" +
+                        getPackageConfig().getMapper().replace(".","/")
+                        + tableInfo.getEntityName() + "Dao" + StringPool.DOT_JAVA;
+            }
+        });
+
+        templatePath = "/templates/entityDTO.java.ftl";
+
+        // 自定义配置会被优先输出
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return property + "/src/main/" +
+                        getPackageConfig().getParent().replace(".","/")
+                        + "/"
+                        + getPackageConfig().getService().replace(".","/")
+                        + "/dto"
+                        + tableInfo.getEntityName() + "DTO" + StringPool.DOT_JAVA;
+            }
+        });
+
+
+        cfg.setFileOutConfigList(focList);
+        return cfg;
     }
 
     @Bean
@@ -56,6 +128,8 @@ public class CodeGeneratorConfig {
         // 配置模板
         TemplateConfig templateConfig = new TemplateConfig();
 
+        templateConfig.setService("/templates/myservice.java");
+        templateConfig.setServiceImpl("/templates/myserviceImpl.java");
         templateConfig.setXml(null);
         return templateConfig;
    }
@@ -67,12 +141,14 @@ public class CodeGeneratorConfig {
        strategy.setNaming(NamingStrategy.underline_to_camel);
        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
        strategy.setSuperEntityClass("com.yu.domain.SystemEntity");
+       strategy.setSuperServiceClass("com.yu.service.IService");
+       strategy.setSuperServiceImplClass("com.yu.service.impl.ServiceImpl");
        strategy.setEntityLombokModel(true);
        strategy.setRestControllerStyle(true);
        // 公共父类
        //strategy.setSuperControllerClass("com.baomidou.ant.common.BaseController");
        // 写于父类中的公共字段
-       strategy.setSuperEntityColumns("id");
+       strategy.setSuperEntityColumns("id","def_user", "def_user_name","def_dt","upd_user","upd_user_name","upd_dt");
        strategy.setInclude("mcr_t_dict_item");
        strategy.setControllerMappingHyphenStyle(true);
        //strategy.setTablePrefix(pc.getModuleName() + "_");
--- src/main/java/com/yu/service/CodeGeneratorSerive.java
+++ src/main/java/com/yu/service/CodeGeneratorSerive.java
@@ -36,6 +36,9 @@ public class CodeGeneratorSerive {
     @Autowired
     private StrategyConfig strategyConfig;
 
+    @Autowired
+    private InjectionConfig injectionConfig;
+
     public void getAutoGenerator(){
         String property = System.getProperty("user.dir");
         // 代码生成器
@@ -43,41 +46,9 @@ public class CodeGeneratorSerive {
         mpg.setGlobalConfig(globalConfig);
         mpg.setDataSource(dataSourceConfig);
         mpg.setPackageInfo(packageConfig);
-        // 自定义配置
-        InjectionConfig cfg = new InjectionConfig() {
-            @Override
-            public void initMap() {
-                // to do nothing
-            }
-        };
 
-        // 如果模板引擎是 freemarker
-        String templatePath = "/templates/mapper.xml.ftl";
-        // 如果模板引擎是 velocity
-        // String templatePath = "/templates/mapper.xml.vm";
 
-        // 自定义输出配置
-        List<FileOutConfig> focList = new ArrayList<>();
-        // 自定义配置会被优先输出
-        focList.add(new FileOutConfig(templatePath) {
-            @Override
-            public String outputFile(TableInfo tableInfo) {
-                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
-                return property + "/src/main/resources/mapper/"  + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
-            }
-        });
-        /*
-        cfg.setFileCreate(new IFileCreate() {
-            @Override
-            public boolean isCreate(ConfigBuilder configBuilder, FileType fileType, String filePath) {
-                // 判断自定义文件夹是否需要创建
-                checkDir("调用默认方法创建的目录");
-                return false;
-            }
-        });
-        */
-        cfg.setFileOutConfigList(focList);
-        mpg.setCfg(cfg);
+        mpg.setCfg(injectionConfig);
 
         mpg.setTemplate(templateConfig);
         mpg.setStrategy(strategyConfig);
@@ -110,7 +81,7 @@ public class CodeGeneratorSerive {
         // 包配置
         PackageConfig pc = new PackageConfig();
         pc.setModuleName("");
-        pc.setParent("com.baomidou.ant");
+        pc.setParent("com.yu");
         mpg.setPackageInfo(pc);
 
         // 自定义配置
@@ -167,6 +138,8 @@ public class CodeGeneratorSerive {
         strategy.setNaming(NamingStrategy.underline_to_camel);
         strategy.setColumnNaming(NamingStrategy.underline_to_camel);
         strategy.setSuperEntityClass("com.yu.domain.SystemEntity");
+        strategy.setSuperServiceClass("com.yu.service.IService");
+        strategy.setSuperServiceImplClass("com.yu.service.impl.ServiceImpl");
         strategy.setEntityLombokModel(true);
         strategy.setRestControllerStyle(true);
         // 公共父类
